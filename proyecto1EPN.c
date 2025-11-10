ae804783c201d3a41e2d5c018e08ab60e3c02714
Matthew Llerena <matthew.llerena@epn.edu.ec>
2025-11-10 14:38:32 -0500

    Avance proyecto1EPN

--- proyecto1EPN.c
+++ proyecto1EPN.c
@@ -31,40 +31,6 @@ int main(void){
 
     
     
-    printf("Digite el numero de opcion a hacer: \n");
-    printf("1. Sumar dos numeros\n");
-    printf("2. Restar dos numeros\n");
-    printf(">>");
-    scanf("%d",&opcion);
-
-    switch (opcion) {
-    case 1:
-
-        int num1, num2;
-
-        printf("Digite el primer numero: \n");
-        scanf("%d",&num1);
-        printf("Digite el otro numero: \n");
-        scanf("%d",&num2);
-
-        printf("La suma de ambos numeros es: %d", num1 + num2);
-
-        break;
-
-    case 2:
-
-        printf("Digite dos numeros para restarlos: \n");
-
-        break;
-    
-    default:
-        break;
-    }
-    
-
-
-
-    
-
     return 0;
+    
 }
