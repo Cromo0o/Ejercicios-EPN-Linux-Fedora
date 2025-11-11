cca17da35e28bf4db838c4b0173fb13613cd604e
Matthew Llerena <matthew.llerena@epn.edu.ec>
2025-11-11 14:53:06 -0500

    Ejercicio 2: Numeros pares del 0 al 10 - bucle for

---
+++ sentenciasIterativasEjercicio2.c
@@ -0,0 +1,15 @@
+//imprimir numeros pares del 0 al 10
+#include <stdio.h>
+
+int main(void){
+
+   int i;
+
+   for ( i = 0; i <= 10; i = i + 2)
+   {
+        printf("%d\n", i);
+   }
+   
+
+    return 0;
+}
