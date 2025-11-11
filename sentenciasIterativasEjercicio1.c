d0f61bb60b4506cce7c315d6ff9d16eed1d05308
Matthew Llerena <matthew.llerena@epn.edu.ec>
2025-11-11 14:47:08 -0500

    Ejercicio 1: Sentencia iterativa - print 0 to 4

---
+++ sentenciasIterativasEjercicio1.c
@@ -0,0 +1,16 @@
+//imprimir numeros del 0 al 4
+#include <stdio.h>
+
+int main(void){
+
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    printf("%d\n", i);
+
+    return 0;
+}
